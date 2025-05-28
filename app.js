49f118a0a3b659e112709fc5a059c59eb980c9d1
Manita-Gurung <07230036.sherubtse@rub.edu.bt>
2025-05-28 12:06:09 +0600

    Deployment

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+const { createUserTable } = require('./models/userModel');
+const adminRoutes = require('./routes/adminRoutes');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const { createFoodTable } = require('./models/foodModel');
+
+const app = express();
+app.use(express.static('public/CSS'));
+const PORT = process.env.PORT || 3000;
+
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secretkey',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Routes
+app.use('/', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+
+
+// Schema creation
+createUserTable(); // Call this after setting up middlewares
+createFoodTable();
+
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
